b00ccb7aec1b29349005337a639ea32264f8c638
ESCOBAR-ANNER <aescobarc13@miumg.edu.gt>
2023-08-12 08:34:35 -0600

    Ejercicio nuemro 1 de la clase 5

---
+++ script.js
@@ -0,0 +1,29 @@
+let add = document.querySelector("#add");
+let substract = document.querySelector("#substract");
+
+
+
+add.addEventListener("click", function () {
+  let output = document.querySelector("#output");
+  let resultado = parseInt(output.innerText) + 1;
+
+
+  if (resultado <=10) {
+    output.innerText = resultado;
+  }
+  else{
+    alert("LLegaste al limite del contador, no puedes ir arriba de 10")
+  }
+});
+
+
+substract.addEventListener("click", function () {
+  let output = document.querySelector("#output");
+  let resultado = parseInt(output.innerText) - 1;
+
+  if (resultado >= 0) {
+    output.innerText = resultado;
+  }else{
+    alert("LLegaste al limite del contador, no puedes ir abajo de cero")
+  }
+});
